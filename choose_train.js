f76ad930e3fac59215f8dcfd002d0b842a0c7907
dwicahjo <dwicahjo7@gmail.com>
2017-09-08 01:47:20 +0700

    add by departure time

--- choose_train.js
+++ choose_train.js
@@ -10,6 +10,7 @@ if(available_train>0){
 	find_by_lowest_price();
 	// find_multiple_seat(4);
 	// find_multiple_seat_and_by_name(2)
+	// find_multiple_seat_and_by_departure_time(2)
 }
 
 //  Refresher
@@ -63,7 +64,25 @@ function find_multiple_seat(seat){
 //  Function to find ticket by multiple person and by name
 function find_multiple_seat_and_by_name(seat){
 	for(i=0; i<available_train;i++){
-		if(($('.itButton')[i].form.train_name.value == desired_train_name) && ($('.itButton')[0].form.ticket_seat.value >= seat)){
+		if(($('.itButton')[i].form.train_name.value == desired_train_name) && ($('.itButton')[i].form.ticket_seat.value >= seat)){
+			array_of_trains.push($('.itButton')[i]);
+		}		
+	}
+	if(array_of_trains.length == 0){
+		setInterval(function(){
+		$('#input').submit();
+		}, 5000);
+	}else{
+	array_of_trains.sort(compare);
+	array_of_trains[0].click();
+	}
+}
+
+// Function to find ticket by multiple person and by departure time
+function find_multiple_seat_and_by_departure_time(seat){
+	for(i=0; i<available_train;i++){
+		for(j=0; j<departure_time.length; j++)
+		if(($('.itButton')[i].form.train_dep_time.value == departure_time[j]) && ($('.itButton')[i].form.ticket_seat.value >= seat)){
 			array_of_trains.push($('.itButton')[i]);
 		}		
 	}
